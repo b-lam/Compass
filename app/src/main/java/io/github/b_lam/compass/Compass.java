9304bc07b1057c439d967f11dd89cdb6bca65e7f
brandon <bclam@uwaterloo.ca>
2016-06-25 19:06:34 -0400

    Added calibrate dialog and blank settings activity

--- app/src/main/java/io/github/b_lam/compass/Compass.java
+++ app/src/main/java/io/github/b_lam/compass/Compass.java
@@ -1,25 +1,36 @@
 package io.github.b_lam.compass;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Compass extends AppCompatActivity {
 
     SensorManager sensorManager;
     SensorEventListener eListener;
     Sensor accelerometer, magFieldSensor;
+    AlertDialog dialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +43,7 @@ public class Compass extends AppCompatActivity {
 
         ImageView imgCompass = new ImageView(getApplicationContext());
         imgCompass.setImageResource(R.drawable.compass);
-        imgCompass.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        imgCompass.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         imgCompass.setAdjustViewBounds(true);
 
         TextView tvDegree = new TextView(getApplicationContext());
@@ -72,5 +83,46 @@ public class Compass extends AppCompatActivity {
         super.onResume();
         sensorManager.registerListener(eListener, accelerometer, SensorManager.SENSOR_DELAY_GAME);
         sensorManager.registerListener(eListener, magFieldSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_calibrate:
+                calibrateDialog();
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent(this, Settings.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void calibrateDialog(){
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View calibrateView = layoutInflater.inflate(R.layout.calibrate_dialog_layout, null);
+
+        dialogBuilder = new AlertDialog.Builder(this).create();
+        dialogBuilder.setTitle("How to calibrate");
+        dialogBuilder.setIcon(R.mipmap.ic_launcher);
+        dialogBuilder.setView(calibrateView);
+
+        Button btnClose = (Button) calibrateView.findViewById(R.id.close);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogBuilder.dismiss();
+            }
+        });
+
+        dialogBuilder.show();
     }
 }
